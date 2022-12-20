b7212c063a88eb08fa4a774e72a826d8bb403ea7
Mateus Augusto <73893960+mateusaugust@users.noreply.github.com>
2022-12-20 11:10:05 -0300

    Add files via upload

---
+++ BEE2791.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main (){
+
+	int j;
+	
+	for(int i =1; i <= 4; i ++){
+		cin >> j;
+		if(j == 1){
+			cout << i << endl;
+		}
+	}
+
+	return 0;
+}
+
